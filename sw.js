86aee2b20b7f46c5de41ea3d11cc3729481fa77d
()鱼子酱) <kbolton@163.com>
2020-04-30 15:56:59 +0800

    update

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
-importScripts('/_nuxt/workbox.4c4f5ca6.js')
+importScripts('/thscom/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/4959fc7849c2a71057f5.js",
+    "url": "/thscom/_nuxt/3fdd2b54b614d82ac6c9.js",
+    "revision": "fc30e30a9e124ca0b63e73fb100f2170"
+  },
+  {
+    "url": "/thscom/_nuxt/4959fc7849c2a71057f5.js",
     "revision": "53287641bd56b9dfe61ad359019a97a1"
   },
   {
-    "url": "/_nuxt/72612560780133e8e830.js",
-    "revision": "fb9330c49127e21d6e4dd1c6e42838ba"
-  },
-  {
-    "url": "/_nuxt/7ea322838e270741f49d.js",
+    "url": "/thscom/_nuxt/7ea322838e270741f49d.js",
     "revision": "d03715c74aa3901672012113de83f61f"
   },
   {
-    "url": "/_nuxt/cd080fd6667a87d5f1b1.js",
-    "revision": "80af71d5af52c09d9882c30589fa9497"
+    "url": "/thscom/_nuxt/d9737b71e1d6be3a6859.js",
+    "revision": "4a1fbd6c34b6fd74c66328d09f3b3183"
   },
   {
-    "url": "/_nuxt/e9f7478ad5c8ce9f59cc.js",
+    "url": "/thscom/_nuxt/e9f7478ad5c8ce9f59cc.js",
     "revision": "0c5a81bee1493af196caa1afb65421f0"
   }
 ], {
@@ -30,6 +30,6 @@ workbox.precaching.precacheAndRoute([
 workbox.clientsClaim()
 workbox.skipWaiting()
 
-workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/thscom/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
 
-workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/thscom/.*'), workbox.strategies.networkFirst({}), 'GET')
