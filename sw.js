d06e554826d83c3c31424f26555e92b70382081e
()鱼子酱) <kbolton@163.com>
2020-05-07 17:45:28 +0800

    更新：PC端和移动端兼容

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ importScripts('/thscom/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/thscom/_nuxt/3eb3da3218c72bda91d5.js",
-    "revision": "57e48f6735609364f8ce4ea915246537"
+    "url": "/thscom/_nuxt/40201ab4b303f8f3bcbf.js",
+    "revision": "e34673c5c27df89245fdb7033646a3ba"
   },
   {
-    "url": "/thscom/_nuxt/5cb8ec743413ddf27e20.js",
-    "revision": "78a75a9662e96913ac8d352f4e52635e"
+    "url": "/thscom/_nuxt/66dfa406addc2eff8834.js",
+    "revision": "74082e7834805be61e0d524f6f9af6e2"
   },
   {
-    "url": "/thscom/_nuxt/7e2e11cfffd7e4a0221a.js",
-    "revision": "ac69e7876156a083f95b2bbd6ee7ed73"
+    "url": "/thscom/_nuxt/859c03f944538b504b4c.js",
+    "revision": "88bd558bf2ad6afa83b041586a643f88"
   },
   {
-    "url": "/thscom/_nuxt/b67452d2542ca7c7e0aa.js",
-    "revision": "022d93a8ccd412e1e9d9066046abb2ea"
+    "url": "/thscom/_nuxt/8a42b509a9e912426bd5.js",
+    "revision": "1d36608bbf6b7326ab2fda0e4033bae9"
   },
   {
-    "url": "/thscom/_nuxt/d3aa9f1d9ecb5118a811.js",
-    "revision": "a32b240a21b87881cbd2061835ce57b0"
+    "url": "/thscom/_nuxt/cbc3a9097b49690f5578.js",
+    "revision": "0cfd45bf37ed0f4bef10695da6bb8f26"
   }
 ], {
   "cacheId": "example-nuxtjs",
