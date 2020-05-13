40151543d428e89760ccbd5868341e9cdcc53006
()鱼子酱) <kbolton@163.com>
2020-05-13 10:48:32 +0800

    修改主题图片不显示的问题

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@ importScripts('/thscom/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/thscom/_nuxt/24ab14e04a971ba82f79.js",
-    "revision": "ccb586eaef5a5dbe5f702107e14a5ae1"
-  },
-  {
     "url": "/thscom/_nuxt/40201ab4b303f8f3bcbf.js",
     "revision": "e34673c5c27df89245fdb7033646a3ba"
   },
   {
-    "url": "/thscom/_nuxt/519799730329612b8fe1.js",
-    "revision": "10de8adfc638b55f9173dfd27f0ab19a"
-  },
-  {
     "url": "/thscom/_nuxt/8f5399281a912c170ae4.js",
     "revision": "a93ef86e99f39d8efce752007c4ab29f"
+  },
+  {
+    "url": "/thscom/_nuxt/c24b1eae7aa7ec38a961.js",
+    "revision": "371698a2eed342ce5560456b28379ebc"
+  },
+  {
+    "url": "/thscom/_nuxt/c7baaf1c6b0eb103e865.js",
+    "revision": "b5a71c73571ff66b5c57f5f31d1838d5"
   },
   {
     "url": "/thscom/_nuxt/cbc3a9097b49690f5578.js",
