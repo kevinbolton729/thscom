fd0a74978a9b12e364b7b7ae859ffabfaa4534b8
()鱼子酱) <kbolton@163.com>
2020-05-13 10:20:28 +0800

    更新网站标题等

--- sw.js
+++ sw.js
@@ -2,27 +2,27 @@ importScripts('/thscom/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
+    "url": "/thscom/_nuxt/24ab14e04a971ba82f79.js",
+    "revision": "ccb586eaef5a5dbe5f702107e14a5ae1"
+  },
+  {
     "url": "/thscom/_nuxt/40201ab4b303f8f3bcbf.js",
     "revision": "e34673c5c27df89245fdb7033646a3ba"
   },
   {
-    "url": "/thscom/_nuxt/66dfa406addc2eff8834.js",
-    "revision": "74082e7834805be61e0d524f6f9af6e2"
+    "url": "/thscom/_nuxt/519799730329612b8fe1.js",
+    "revision": "10de8adfc638b55f9173dfd27f0ab19a"
   },
   {
-    "url": "/thscom/_nuxt/859c03f944538b504b4c.js",
-    "revision": "88bd558bf2ad6afa83b041586a643f88"
-  },
-  {
-    "url": "/thscom/_nuxt/8a42b509a9e912426bd5.js",
-    "revision": "1d36608bbf6b7326ab2fda0e4033bae9"
+    "url": "/thscom/_nuxt/8f5399281a912c170ae4.js",
+    "revision": "a93ef86e99f39d8efce752007c4ab29f"
   },
   {
     "url": "/thscom/_nuxt/cbc3a9097b49690f5578.js",
     "revision": "0cfd45bf37ed0f4bef10695da6bb8f26"
   }
 ], {
-  "cacheId": "example-nuxtjs",
+  "cacheId": "thscd",
   "directoryIndex": "/",
   "cleanUrls": false
 })
