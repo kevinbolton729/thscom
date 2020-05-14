62d7e95b6295bbca650f9c5e283bc22bfc323287
()鱼子酱) <kbolton@163.com>
2020-05-14 16:26:08 +0800

    添加发送邮件的模块
    TODO：还未调用邮件发送的接口，完成此功能

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ importScripts('/thscom/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/thscom/_nuxt/1f49b385cf6f1a7ba1d3.js",
-    "revision": "6f8f01062e5ef6f61bb12baf4fb4cf11"
+    "url": "/thscom/_nuxt/017f37aac7263697e632.js",
+    "revision": "346b4fcd58c42d2a7246b746bf40f358"
   },
   {
-    "url": "/thscom/_nuxt/2f432058f891fc3815a6.js",
-    "revision": "12046724761f4b2fa676f3774e4bfddb"
+    "url": "/thscom/_nuxt/287f93c8b666bfb4b74a.js",
+    "revision": "7d6204f6307b140a7a83642fc2f1b5cf"
   },
   {
-    "url": "/thscom/_nuxt/35eb66c942c905819616.js",
-    "revision": "3116f535e0800f17e98dc4e5dfe0471c"
+    "url": "/thscom/_nuxt/2bbe7c20c1476027a7b8.js",
+    "revision": "cfcb011feb2257b3911470aa40529316"
   },
   {
-    "url": "/thscom/_nuxt/40201ab4b303f8f3bcbf.js",
-    "revision": "e34673c5c27df89245fdb7033646a3ba"
+    "url": "/thscom/_nuxt/395464d168cdd55dff42.js",
+    "revision": "e73e8e4d1aa02b22910290d829ff4496"
   },
   {
-    "url": "/thscom/_nuxt/cbc3a9097b49690f5578.js",
-    "revision": "0cfd45bf37ed0f4bef10695da6bb8f26"
+    "url": "/thscom/_nuxt/98a991df91c8e64a136c.js",
+    "revision": "1bd26e90b696b54c4e60c4b30f884e9a"
   }
 ], {
   "cacheId": "thscd",
