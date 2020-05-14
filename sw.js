65992b72b77527908de52dcd30e7ef8c1247e5a9
()鱼子酱) <kbolton@163.com>
2020-05-14 17:12:00 +0800

    将图片使用 van-image 组件封装

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@ importScripts('/thscom/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/thscom/_nuxt/017f37aac7263697e632.js",
-    "revision": "346b4fcd58c42d2a7246b746bf40f358"
+    "url": "/thscom/_nuxt/00e4d018b0012cd813f9.js",
+    "revision": "74dd19be2c2e7726d4c6ec8d4163fb91"
   },
   {
-    "url": "/thscom/_nuxt/287f93c8b666bfb4b74a.js",
-    "revision": "7d6204f6307b140a7a83642fc2f1b5cf"
+    "url": "/thscom/_nuxt/017f37aac7263697e632.js",
+    "revision": "346b4fcd58c42d2a7246b746bf40f358"
   },
   {
     "url": "/thscom/_nuxt/2bbe7c20c1476027a7b8.js",
     "revision": "cfcb011feb2257b3911470aa40529316"
   },
   {
-    "url": "/thscom/_nuxt/395464d168cdd55dff42.js",
-    "revision": "e73e8e4d1aa02b22910290d829ff4496"
+    "url": "/thscom/_nuxt/755927108ab8780cbe81.js",
+    "revision": "2b2ac760b610b3eb158f16f21a95de7a"
   },
   {
     "url": "/thscom/_nuxt/98a991df91c8e64a136c.js",
